2dccd28492f506dc21539d42dc655e45535311ed
South <1275093225@qq.com>
2022-11-06 15:37:29 +0800

    wip: 读取文件信息

--- src/lib/analyzer.ts
+++ src/lib/analyzer.ts
@@ -1,5 +1,15 @@
-import puppeteer from 'puppeteer'
 import { getLoginInfo } from '../utils/inquirer'
+import { hasFile } from '../utils/helper'
+import puppeteer from 'puppeteer'
+import path from 'path'
+import fs from 'fs-extra'
+
+const passwordFilePath = path.join(__dirname, './password_file.ts')
+
+interface IPasswordFileResult {
+  username: string;
+  password: string;
+}
 
 /**
  * 根据链接获取界面数据
@@ -51,6 +61,50 @@ class Analyzer {
   }
 
   /**
+   * 保存密码文件
+   * @param con - 文件内容
+   */
+  private savePasswordFile(con: string) {
+    // 保存密码文件不存在则创建
+    if (!hasFile(passwordFilePath)) fs.createFileSync(passwordFilePath)
+    // 保存文件数据
+    fs.writeFileSync(passwordFilePath, con)
+  }
+
+  /**
+   * 获取数组中数据
+   * @param str - 数组
+   */
+  private getStrData(str: string) {
+    const usernameStr = str.substring(0, 9),
+          usernameArr = usernameStr.split(';'),
+          username = usernameArr?.[0] || '',
+          passwordStr = usernameStr.substring(0, 9),
+          passwordArr = passwordStr.split(';'),
+          password = passwordArr?.[0] || ''
+
+    return { username, password }
+  }
+
+  /** 读取文件数据 */
+  private getPasswordFile(): IPasswordFileResult {
+    if (!hasFile(passwordFilePath)) {
+      return { username: '', password: '' }
+    }
+    const code = fs.readFileSync(passwordFilePath).toString()
+    const { username, password } = this.getStrData(code)
+    console.log('{ username, password }:', { username, password })
+
+    return { username, password }
+  }
+
+  /** 删除密码文件 */
+  private removePasswordFile() {
+    // 保存密码文件存在则删除
+    if (hasFile(passwordFilePath)) fs.removeSync(passwordFilePath)
+  }
+
+  /**
    * 输入账号/密码
    * @param username - 账号
    * @param password - 密码
@@ -62,12 +116,22 @@ class Analyzer {
     page: puppeteer.Page
   ) {
     try {
+      const code = this.getPasswordFile()
+      console.log('code:', code)
+      // 删除密码文件
+      this.removePasswordFile()
+
+      // 输入账号和密码，并登录
       const usernameLabel = '#email', passwordLabel = '#password'
       await page.waitForSelector(usernameLabel, { timeout: 5000 })
       await page.type(usernameLabel, username)
       await page.waitForSelector(passwordLabel, { timeout: 5000 })
       await page.type(passwordLabel, password)
       await page.click('button[type="submit"]')
+
+      // 判断页面成功并保存密码文件
+      const con = `username:${username};\npassword:${password};\n`
+      this.savePasswordFile(con)
     } catch(err) {
       console.log('输入账号密码失败')
     }
@@ -88,9 +152,7 @@ class Analyzer {
       await this.inputPassword(username, password, page)
       console.log(username, password)
     }
-    console.log('isPassword:', isPassword)
   }
-
   
   /**
    * 获取标题
